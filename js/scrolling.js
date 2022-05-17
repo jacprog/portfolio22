813768ccc6916ad08e326e84355cf4b9cec981ab
Jacques Favre <jacprog@users.noreply.github.com>
2022-05-16 20:14:52 -0400

    Add files via upload

---
+++ js/scrolling.js
@@ -0,0 +1,9 @@
+$(function() {
+    // Initialize Skrollr
+    var s = skrollr.init({
+        render: function(data) {
+            //Uncomment the following line to turn on debugging logs
+            //console.log(data.curTop);
+        }
+    });
+});
